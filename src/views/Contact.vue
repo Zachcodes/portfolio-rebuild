08eeaed1f1869d8338a1b2fcc8a4d0855eba1547
zach <zaspringer@gmail.com>
2018-09-14 16:50:21 -0600

    Finished for the day. Just got started on the contactform

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="contact">
+  <div class="contact-main-container">
+    <form class="contact-form-container">
+      <label>Name</label> <input v-bind="test"/>
+    </form>
     <SocialMediaContainer/>
   </div>
 </template>
@@ -9,8 +12,25 @@ import SocialMediaContainer from '../components/SocialMediaContainer.vue'
 
 export default {
   name: 'contact',
+  data: {
+    test: 'hello'
+  },
   components: {
     SocialMediaContainer
   }
 }
 </script>
+
+<style>
+.contact-main-container {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.contact-form-container {
+  width: 40%;
+  height: 60%;
+}
+</style>
