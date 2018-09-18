fb9fdceae9e1b3c3f100ff48062ce391561d74c0
zach <zaspringer@gmail.com>
2018-09-18 14:56:46 -0600

    Got email to work. Need to figure it out on the front end now

--- server/index.py
+++ server/index.py
@@ -1,6 +1,19 @@
 from flask import Flask 
 from flask_cors import CORS
 from flask import request
+from dotenv import load_dotenv
+load_dotenv() 
+import os 
+HOST_EMAIL = os.getenv("HOST_EMAIL")
+HOST_PASSWORD = os.getenv("HOST_PASSWORD")
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+s.starttls()
+s.login(HOST_EMAIL, HOST_PASSWORD)
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +21,20 @@ CORS(app)
 @app.route('/api/submitContact', methods=['POST'])
 def handle_submit():
     jsonData = request.get_json()
-    return 'working I guess'
+    name = jsonData['name']
+    email = jsonData['email']
+    message = jsonData['message']
+    msg = MIMEMultipart()
+    msg['From'] = email;
+    msg['To'] = HOST_EMAIL;
+    msg['Subject'] = email
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    s.send_message(msg)
+
+    del msg;
+    return 'Successfully sent message'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
