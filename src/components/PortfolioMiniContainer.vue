746fb423d6aa9666e39d0d15b1b14eb60a05c183
zach <zaspringer@gmail.com>
2018-09-21 16:56:59 -0600

    Working on the portfolio mini container. done for the day

--- src/components/PortfolioMiniContainer.vue
+++ src/components/PortfolioMiniContainer.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="portfolio-mini-container">
-    {{projectId}}
+  <div class="portfolio-mini-container" v-on:mouseover="setHover">
+    <button class="view-more-button" ref="viewMore">View More</button>
+    <div class="portfolio-mini-container-film" ref="film"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'portfolioMiniContainer',
-  props: ['projectId']
+  props: ['projectId'],
+  methods: {
+      setHover() {
+          let {film, viewMore} = this.$refs;
+          film.classList.add('show-on-hover')
+          viewMore.classList.add('show-on-hover')
+      }
+  }
 }
 </script>
 
@@ -17,5 +25,19 @@ export default {
     width: 300px;
     background-color: white;
     margin-bottom: 20px;
+    position: relative;
+}
+.view-more-button {
+    display: none;
+}
+.portfolio-mini-container-film {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-color: grey;
+    display: none;
+}
+.show-on-hover {
+    display: inline-block;
 }
 </style>
