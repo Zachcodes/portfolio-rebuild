b9a8a8768277a5c1eac02aad248ae0ca9fe6862a
zach <zaspringer@gmail.com>
2018-09-26 10:21:46 -0600

    Working on the layout of the project view

--- server/JsUtilities/formatProject.js
+++ server/JsUtilities/formatProject.js
@@ -2,7 +2,11 @@ module.exports = function(projectRecords) {
     let project = projectRecords.reduce((accum, currVal) => {
         if(!accum.id) accum.id = currVal.id;
         if(!accum.pictureLinks) accum.pictureLinks = [];
-        accum.pictureLinks.push(currVal.link);
+        let linkObj = {}
+        linkObj.link = currVal.link
+        linkObj.type_id = currVal.type_id 
+        linkObj.project_image_id = currVal.project_image_id
+        accum.pictureLinks.push(linkObj);
         if(!accum.description) accum.description = currVal.description;
         if(!accum.name) accum.name = currVal.name
         return accum;
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        <span>{{projectDetails.description}}</span>
+    <div class="project-main-container">
+        <div class="project-pictures-main-container">
+            <div class="project-picture-main" :style="mainDisplayedImage"></div> 
+            <div class="project-picture-sub">
+                <div v-for="picture in projectPictures"
+                     :key="picture.project_image_id"></div>
+            </div>
+        </div>
+        <div class="project-about-main-container">
+            <div class="project-about-description"></div>
+            <div class="project-tech-main-container">
+                <div class="project-tech"></div> 
+                <div class="project-view-button"></div>
+            </div>
+        </div> 
     </div>
 </template>
 
@@ -12,19 +25,91 @@ export default {
     data() {
         return {
             projectDetails: {},
-            projectPictures: []
+            projectPictures: [],
+            startingPictureIndex: 0
+        }
+    },
+    computed: {
+        mainDisplayedImage() {
+            if(this.projectPictures.length) {
+                let link = this.projectPictures[this.startingPictureIndex].link;
+                return {
+                    backgroundImage: `url(${link})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover'
+                }
+            }
         }
     },
     mounted() {
         let {projectId} = this.$route.query
         axios.get(`/api/projects/${projectId}`).then(response => { 
             this.projectDetails = response.data;
-            console.log(response.data.pictureLinks)
-            let sorted = response.data.pictureLinks.sort((a, b) => {
-                if(a.)
+            let sortedByType = response.data.pictureLinks.sort((a, b) => {
+                if(a.type_id === 1) return -1 
+                else return 1
             })
-            this.projectPictures = response.data.pictureLinks;
+            this.projectPictures = sortedByType;
         })
     }
 }
 </script>
+
+<style>
+.project-main-container {
+  width: 100%;
+  min-height: 80vh;
+  display: flex;
+  padding-top: 50px;
+  padding-left: 25px;
+  padding-right: 25px;
+  height: 80vh;
+}
+.project-pictures-main-container {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+}
+.project-picture-main {
+    height: 80%;
+    width: 100%;
+    background-color: white;
+} 
+.project-picture-sub {
+    height: 20%;
+    width: 100%;
+    background-color: silver;
+    display: flex;
+}
+.project-about-main-container {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+}
+.project-about-description {
+    height: 80%;
+    width: 100%;
+    background-color: white;
+}
+.project-tech-main-container {
+    height: 20%;
+    width: 100%;
+    display: flex; 
+    background-color: red;
+}
+.project-tech {
+    width: 60%;
+    height: 100%;
+    background-color: white;
+}
+.project-view-button {
+    width: 40%;
+    height: 100%;
+    background-color: blue;
+}
+</style>
